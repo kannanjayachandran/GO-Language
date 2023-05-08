2255b6b7453b6d06cc5b73446d21e73b6986fc12
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2023-05-08 19:32:17 +0530

    add file

---
+++ 01. Basics/01. Introduction to GO/Switch.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+
+	// normal switch statement
+	switch a {
+	case 1:
+		fmt.Println("case1")
+
+	case 10:
+		fmt.Println("Case2")
+
+	default:
+		fmt.Println("Default switch")
+	}
+
+	switch {
+	case 1 < 2:
+		fmt.Println("Hello GO")
+		fallthrough
+
+	case 100 > 10:
+		fmt.Println("Passed through")
+
+	case 100 < 10:
+		fmt.Println("null")
+
+	}
+
+}
